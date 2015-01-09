4e74db8263447792366638cd0f560b7bd3e24037
haonancool <hnliucool@gmail.com>
2015-01-09 23:18:51 +0800

    Create two_sum.cc

---
+++ leetcode/two_sum.cc
@@ -0,0 +1,18 @@
+class Solution {
+public:
+  vector<int> twoSum(vector<int> &numbers, int target) {
+    int size = numbers.size();
+    unordered_map<int, int> hash;
+		vector<int> ret;
+    for (int i = 0; i < size; i++) {
+      int diff = target - numbers[i];
+			if (hash.find(diff) != hash.end()) {
+				ret.push_back(hash[diff]);
+				ret.push_back(i+1);
+        return ret;
+      }
+      hash[numbers[i]] = i+1;
+    }
+		return ret;
+  }
+};
