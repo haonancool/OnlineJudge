08614b2d187351584b349140981c021f6754bec1
haonancool <hnliucool@gmail.com>
2015-01-09 23:19:50 +0800

    Update two_sum.cc

--- leetcode/two_sum.cc
+++ leetcode/two_sum.cc
@@ -3,16 +3,16 @@ public:
   vector<int> twoSum(vector<int> &numbers, int target) {
     int size = numbers.size();
     unordered_map<int, int> hash;
-		vector<int> ret;
+    vector<int> ret;
     for (int i = 0; i < size; i++) {
       int diff = target - numbers[i];
-			if (hash.find(diff) != hash.end()) {
-				ret.push_back(hash[diff]);
-				ret.push_back(i+1);
+      if (hash.find(diff) != hash.end()) {
+	ret.push_back(hash[diff]);
+	ret.push_back(i+1);
         return ret;
       }
       hash[numbers[i]] = i+1;
     }
-		return ret;
+    return ret;
   }
 };
